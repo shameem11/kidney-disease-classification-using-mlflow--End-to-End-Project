988e640523eb89450dbe580236d65c73c03404d1
https://github.com/shameem11 <Shameemmon.mk@gmail.com>
2024-07-28 17:16:02 +0400

    Data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.kidneyDisease import logger
+from kidneyDisease import logger
+from kidneyDisease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('custom log check ')
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
