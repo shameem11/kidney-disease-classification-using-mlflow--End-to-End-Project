76ee1292a4118ca5be94ae5f8525c10bc64e96ca
https://github.com/shameem11 <Shameemmon.mk@gmail.com>
2024-08-14 20:33:52 +0400

    Model Training and work flow

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kidneyDisease import logger
 from kidneyDisease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from kidneyDisease.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from kidneyDisease.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -26,3 +26,15 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer =ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/kidneyDisease/entity/config_entity.py
+++ src/kidneyDisease/entity/config_entity.py
@@ -23,3 +23,13 @@ class PrepareBaseModelConfig:
 
 
 
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
